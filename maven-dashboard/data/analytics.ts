e85539446e0a4f058689280d507894e7171f9355
Eleven <danielulelu@gmail.com>
2024-07-10 13:35:08 +0300

    data for the whole year

---
+++ maven-dashboard/data/analytics.ts
@@ -0,0 +1,76 @@
+import { AnalyticsItem } from "./../types/analytics";
+
+const data: AnalyticsItem[] = [
+  {
+    name: "Jan",
+    uv: 400,
+    pv: 1000,
+    amt: 2400,
+  },
+  {
+    name: "Feb",
+    uv: 300,
+    pv: 800,
+    amt: 2210,
+  },
+  {
+    name: "Mar",
+    uv: 200,
+    pv: 600,
+    amt: 1700,
+  },
+  {
+    name: "Apr",
+    uv: 100,
+    pv: 400,
+    amt: 2100,
+  },
+  {
+    name: "May",
+    uv: 50,
+    pv: 150,
+    amt: 2500,
+  },
+  {
+    name: "Jun",
+    uv: 300,
+    pv: 900,
+    amt: 2000,
+  },
+  {
+    name: "Jul",
+    uv: 150,
+    pv: 600,
+    amt: 1600,
+  },
+  {
+    name: "Aug",
+    uv: 250,
+    pv: 800,
+    amt: 2200,
+  },
+  {
+    name: "Sep",
+    uv: 100,
+    pv: 300,
+    amt: 2100,
+  },
+  {
+    name: "Oct",
+    uv: 50,
+    pv: 150,
+    amt: 2500,
+  },
+  {
+    name: "Nov",
+    uv: 150,
+    pv: 450,
+    amt: 2100,
+  },
+  {
+    name: "Dec",
+    uv: 200,
+    pv: 500,
+    amt: 1900,
+  },
+];
