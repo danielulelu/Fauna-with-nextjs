3c29c5198679606e807e44d4ed5e84c3a50092c2
Eleven <danielulelu@gmail.com>
2024-07-10 11:41:15 +0300

    Introduces a sidebar component for the dashboard links

---
+++ maven-dashboard/components/Sidebar.tsx
@@ -0,0 +1,58 @@
+import { LayoutDashboard, Folders, CreditCard, Settings, User, Newspaper } from 'lucide-react';
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command";
+import Link from "next/link";
+
+const Sidebar = () => {
+  return (
+    <Command className="bg-secondary rounded-none">
+      <CommandInput placeholder="Type a search..." />
+      <CommandList>
+        <CommandEmpty>No results found.</CommandEmpty>
+        <CommandGroup heading="Suggestions" className="cursor-pointer">
+          <CommandItem>
+            <LayoutDashboard className='mr-2 h-4 w-4'></LayoutDashboard>
+            <Link href='/'>Dashboard</Link>
+          </CommandItem>
+          <CommandItem>
+          <Newspaper className='mr-2 h-4 w-4'></Newspaper>
+          <Link href='/posts'>Posts</Link>
+          </CommandItem>
+          <CommandItem>
+          <Folders className='mr-2 h-4 w-4'></Folders>
+          <Link href='/'>Categories</Link>
+          </CommandItem>
+        </CommandGroup>
+        <CommandSeparator />
+        <CommandGroup heading="Settings">
+          <CommandItem>
+            <User className='mr-2 h-4 w-4'/>
+            <span>Profile</span>
+            <CommandShortcut>Ctrl^Y</CommandShortcut>
+          </CommandItem>
+          <CommandItem>
+          <CreditCard className='mr-2 h-4 w-4'/>
+            <span>Billing</span>
+            <CommandShortcut>Ctrl^B</CommandShortcut>
+          </CommandItem>
+          <CommandItem>
+          <User className='mr-2 h-4 w-4'/>
+            <span>Settings</span>
+            <CommandShortcut>Ctrl^S</CommandShortcut>
+          </CommandItem>
+        </CommandGroup>
+      </CommandList>
+    </Command>
+  );
+};
+
+export default Sidebar;
