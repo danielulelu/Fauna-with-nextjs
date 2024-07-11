4261f35a40d83563a0089b4e28b3dda44d92a549
Eleven <danielulelu@gmail.com>
2024-07-11 23:40:05 +0300

    added ways to capture and sensitise the sidebar more

--- maven-dashboard/components/Sidebar.tsx
+++ maven-dashboard/components/Sidebar.tsx
@@ -1,4 +1,6 @@
-import { LayoutDashboard, Folders, CreditCard, Settings, User, Newspaper } from 'lucide-react';
+"use client";
+import * as React from "react";
+
 import {
   Command,
   CommandDialog,
@@ -10,48 +12,80 @@ import {
   CommandSeparator,
   CommandShortcut,
 } from "@/components/ui/command";
+import {
+  LayoutDashboard,
+  Newspaper,
+  Folders,
+  CreditCard,
+  Settings,
+  User,
+} from "lucide-react";
 import Link from "next/link";
 
 const Sidebar = () => {
+  const [open, setOpen] = React.useState(false);
+
+  React.useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "j" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+    document.addEventListener("keydown", down);
+    return () => {
+      document.removeEventListener("keydown", down);
+    };
+  }, []);
+
   return (
-    <Command className="bg-secondary rounded-none">
-      <CommandInput placeholder="Type a search..." />
-      <CommandList>
-        <CommandEmpty>No results found.</CommandEmpty>
-        <CommandGroup heading="Suggestions" className="cursor-pointer">
-          <CommandItem>
-            <LayoutDashboard className='mr-2 h-4 w-4'></LayoutDashboard>
-            <Link href='/'>Dashboard</Link>
-          </CommandItem>
-          <CommandItem>
-          <Newspaper className='mr-2 h-4 w-4'></Newspaper>
-          <Link href='/posts'>Posts</Link>
-          </CommandItem>
-          <CommandItem>
-          <Folders className='mr-2 h-4 w-4'></Folders>
-          <Link href='/'>Categories</Link>
-          </CommandItem>
-        </CommandGroup>
-        <CommandSeparator />
-        <CommandGroup heading="Settings">
-          <CommandItem>
-            <User className='mr-2 h-4 w-4'/>
-            <span>Profile</span>
-            <CommandShortcut>^Y</CommandShortcut>
-          </CommandItem>
-          <CommandItem>
-          <CreditCard className='mr-2 h-4 w-4'/>
-            <span>Billing</span>
-            <CommandShortcut>^B</CommandShortcut>
-          </CommandItem>
-          <CommandItem>
-          <User className='mr-2 h-4 w-4'/>
-            <span>Settings</span>
-            <CommandShortcut>^S</CommandShortcut>
-          </CommandItem>
-        </CommandGroup>
-      </CommandList>
-    </Command>
+    <>
+      <p className="text-sm text-muted-foreground">
+        Press
+        <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
+          <span className="text-xs">⌘</span>J
+        </kbd> to open the command palette.
+      </p>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <Command className="bg-secondary rounded-none">
+          <CommandInput placeholder="Type a command or search..." />
+          <CommandList>
+            <CommandGroup heading="Suggestions">
+              <CommandItem>
+                <LayoutDashboard className="mr-2 h-4 w-4" />
+                <Link href="/">Dashboard</Link>
+              </CommandItem>
+              <CommandItem>
+                <Newspaper className="mr-2 h-4 w-4" />
+                <Link href="/posts">Posts</Link>
+              </CommandItem>
+              <CommandItem>
+                <Folders className="mr-2 h-4 w-4" />
+                <Link href="#">Categories</Link>
+              </CommandItem>
+            </CommandGroup>
+            <CommandSeparator />
+            <CommandGroup heading="Settings">
+              <CommandItem>
+                <User className="mr-2 h-4 w-4" />
+                <span>Profile</span>
+                <CommandShortcut>⌘P</CommandShortcut>
+              </CommandItem>
+              <CommandItem>
+                <CreditCard className="mr-2 h-4 w-4" />
+                <span>Billing</span>
+                <CommandShortcut>⌘B</CommandShortcut>
+              </CommandItem>
+              <CommandItem>
+                <Settings className="mr-2 h-4 w-4" />
+                <span>Settings</span>
+                <CommandShortcut>⌘S</CommandShortcut>
+              </CommandItem>
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </CommandDialog>
+    </>
   );
 };
 
